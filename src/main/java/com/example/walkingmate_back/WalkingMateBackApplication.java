4f5df60c847d38c25296c97e1826fc1d37d93c63
woojin <jwjley@naver.com>
2023-07-05 13:01:02 +0900

    feat : spring Project 생성 (2023-07-05 우진)

---
+++ src/main/java/com/example/walkingmate_back/WalkingMateBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.walkingmate_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkingMateBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalkingMateBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/walkingmate_back/WalkingMateBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walkingmate_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkingMateBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
